e374bef5990a4383fcd4f9efeeff7afda0f8da4e
andersonstack <andersong.pereiracruz@gmail.com>
2025-09-27 15:02:21 -0300

    refactor: tornando a rota de api mais dinâmica

--- projeto-web/src/app/api/route.ts
+++ projeto-web/src/app/api/route.ts
@@ -1,21 +1,32 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const titleSearchKey = searchParams.get("titleSearchKey") || "bagdad";
-    const page = searchParams.get("page") || "1";
+  const { searchParams } = new URL(request.url);
+  const titleSearchKey = searchParams.get("titleSearchKey");
+  const page = searchParams.get("page") || "1";
+  const id = searchParams.get("id");
 
-    try {
-      const res = await fetch(
-        `http://www.omdbapi.com/?i=tt3896198&apikey=b31c36d8&s=${titleSearchKey}&page=${page}`
-      );
-      const data = await res.json();
+  if (!id && !titleSearchKey) {
+    return NextResponse.json(
+      { error: "Parâmetros insuficientes" },
+      { status: 400 }
+    );
+  };
 
-      return NextResponse.json(data);
-    } catch (error) {
-      return NextResponse.json(
-        { error: "Erro ao buscar dados" },
-        { status: 500 }
-      );
-    }
+  let url = `http://www.omdbapi.com/?apikey=b31c36d8`;
+
+  if (id) url += `&i=${id}`;
+  if (titleSearchKey) url += `&s=${titleSearchKey}&page=${page}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Erro ao buscar dados" },
+      { status: 500 }
+    );
+  }
 }
--- projeto-web/src/app/home/HomeService.tsx
+++ projeto-web/src/app/home/HomeService.tsx
@@ -2,12 +2,14 @@
 import type { Movie } from "../types/movie";
 
 export async function HomeService(
-  page: number,
-  searchKey: string
+  page: number = 1,
+  searchKey: string = "bagdad"
 ): Promise<Movie[]> {
   "use server";
+
   const res = await fetch(
-    `http://localhost:3000/api?titleSearchKey=${encodeURIComponent(searchKey)}&page=${page}`
+    `http://localhost:3000/api?titleSearchKey=${encodeURIComponent(searchKey)}&page=${page}`,
+    {cache: "no-store"}
   );
   const data = await res.json();
   return data?.Search || [];

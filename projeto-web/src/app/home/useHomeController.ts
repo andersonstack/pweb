805f015dd726083409d531b0268f4f0ef32e087b
andersonstack <andersong.pereiracruz@gmail.com>
2025-10-03 15:06:43 -0300

    fix: retirando uso de searchParams em client

--- projeto-web/src/app/home/useHomeController.ts
+++ projeto-web/src/app/home/useHomeController.ts
@@ -1,17 +1,17 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 // Hooks
 import { useSearchMovies } from "./hooks/useSearchMovies";
-import { useSyncUrl } from "./hooks/use-SyncUrl";
+// import { useSyncUrl } from "./hooks/use-SyncUrl";
 
 export function useHomeController() {
-  const searchParams = useSearchParams();
-  const pageFromUrl = parseInt(searchParams.get("page") || "1", 10);
-  const [page, setPage] = useState(pageFromUrl);
-
+  // const searchParams = useSearchParams();
+  // const pageFromUrl = parseInt(searchParams.get("page") || "1", 10);
+  const [page, setPage] = useState(1);
+  // console.log("Mounter HomeController");
   const {movies, loading, search} = useSearchMovies(page);
-  useSyncUrl(page, search);
+  // useSyncUrl(page, search), [page];
 
   return { page, setPage, movies, loading };
 }

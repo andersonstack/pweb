cc84e45a336519a8e2603305b0c19944741e4b1d
andersonstack <andersong.pereiracruz@gmail.com>
2025-10-10 10:14:41 -0300

    feat: criando página do componente de form

---
+++ projeto-web/src/features/form/components/FormView.tsx
@@ -0,0 +1,84 @@
+"use client";
+import Link from "next/link";
+import useHandleSubmit from "../hooks/useHandleSubmit";
+
+export default function FormView() {
+  const { values, handleChange, handleSubmit, posterPreview } = useHandleSubmit(
+    { title: "", year: "", type: "", poster: "" }
+  );
+
+  return (
+    <>
+      <Link href={"/home"}>Voltar para a home</Link>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md mx-auto mt-8 p-6 bg-[#ffd8b5] rounded shadow text-[#5c4a3f]"
+      >
+        <h2 className="text-2xl font-bold mb-4 text-[#5c4a3f]">
+          Adicionar Filme
+        </h2>
+        <div className="mb-4">
+          <label className="block mb-1 font-medium text-[#5c4a3f]">
+            Título
+          </label>
+          <input
+            type="text"
+            name="title"
+            value={values.title}
+            onChange={handleChange}
+            className="w-full border rounded px-3 py-2 text-[#5c4a3f] bg-white"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1 font-medium text-[#5c4a3f]">Ano</label>
+          <input
+            type="text"
+            name="year"
+            value={values.year}
+            onChange={handleChange}
+            className="w-full border rounded px-3 py-2 text-[#5c4a3f] bg-white"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1 font-medium text-[#5c4a3f]">Tipo</label>
+          <input
+            type="text"
+            name="type"
+            value={values.type}
+            onChange={handleChange}
+            className="w-full border rounded px-3 py-2 text-[#5c4a3f] bg-white"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1 font-medium text-[#5c4a3f]">
+            Poster
+          </label>
+          <input
+            type="text"
+            placeholder="URL da imagem"
+            name="poster"
+            value={values.poster}
+            onChange={handleChange}
+            className="w-full rounded px-3 py-2 text-[#5c4a3f] bg-white"
+          />
+          {posterPreview && (
+            <img
+              src={values.poster}
+              alt="Pré-visualização do Poster"
+              className="mt-2 w-40 h-60 object-cover rounded shadow border"
+            />
+          )}
+        </div>
+        <button
+          type="submit"
+          className="bg-[#5c4a3f] text-[#ffd8b5] px-4 py-2 rounded hover:bg-[#7a5e4a]"
+        >
+          Salvar
+        </button>
+      </form>
+    </>
+  );
+}
